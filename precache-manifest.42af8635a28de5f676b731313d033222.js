0c2177b102e1406743c9d9f8434d5b76d7e0e22b
Maluzzz <maaluuz@gmail.com>
2020-10-21 21:36:51 +0200

    Updates

---
+++ precache-manifest.42af8635a28de5f676b731313d033222.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2be95d97352adb7a56fafa4377bcb9f",
+    "url": "/my-simple-components/index.html"
+  },
+  {
+    "revision": "e4d475ce4ead77984196",
+    "url": "/my-simple-components/static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "be7c40eaa699956be896",
+    "url": "/my-simple-components/static/js/2.77a70c0e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my-simple-components/static/js/2.77a70c0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d475ce4ead77984196",
+    "url": "/my-simple-components/static/js/main.9cea6c01.chunk.js"
+  },
+  {
+    "revision": "127e28e8263cbdeb3091",
+    "url": "/my-simple-components/static/js/runtime-main.c3908141.js"
+  }
+]);
